8e9ff8029070e441b0ad8f43f3a986c8a6fa01cb
humaiun <humaiunkobirr@gmail.com>
2023-09-03 02:34:03 +0600

    added service details page

--- src/pages/serviceDetails/ServiceDetails.jsx
+++ src/pages/serviceDetails/ServiceDetails.jsx
@@ -1,20 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import SubBanner from "../shared/subBanner/SubBanner";
 import banner from "./../../assets/images/banner/1.jpg";
 import { FiPlayCircle } from "react-icons/fi";
 import { useContext } from "react";
 import { ServiceContext } from "../../contexts/ServiceProvider";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { BsFileEarmarkBarGraph } from "react-icons/bs";
+import logo from "./../../assets/logo.svg";
 
 const ServiceDetails = () => {
   const service = useLoaderData();
-  const { img, description, title, facility } = service;
-  const service1 = useContext(ServiceContext);
-  console.log(service1);
+  const { services } = useContext(ServiceContext);
+  const { img, description, title, facility, price } = service;
 
   return (
     <div>
       <SubBanner banner={banner} pageName={"Service Details"} />
-      <div className="grid grid-cols-3 gap-4 my-32">
+      <div className="grid grid-cols-3 gap-6 my-32">
         <div className="col-span-2">
           <img className="w-full rounded-lg" src={img} alt="" />
           <h1 className="mt-12 mb-8 text-[#151515] text-4xl font-bold">{title}</h1>
@@ -74,7 +76,62 @@ const ServiceDetails = () => {
           </div>
         </div>
         <div className="">
-          <div></div>
+          <div className="bg-[#F3F3F3] text-[#151515] p-10 rounded">
+            <h3 className=" text-2xl font-bold mb-5">Services</h3>
+            {services.map((service) => (
+              <div
+                key={service._id}
+                className="font-semibold text-lg flex items-center justify-between rounded mb-5 p-5 hover:bg-[#ff391180] hover:text-white duration-300"
+              >
+                <h5>{service.title}</h5>
+                <Link to={`/service-details/${service._id}`}>
+                  <AiOutlineArrowRight className="text-[#FF3811] text-2xl" />
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className="bg-[#151515] text-[#FFF] p-10 rounded-xl my-8">
+            <h3 className=" text-2xl font-bold mb-5">Download</h3>
+            <div className="flex items-center justify-around">
+              <BsFileEarmarkBarGraph className="text-3xl" />
+              <div>
+                <h5 className="text-lg font-semibold">Our Brochure</h5>
+                <p className="text-[#A2A2A2]">Download</p>
+              </div>
+              <div className="w-14 h-14 bg-[#FF3811] rounded flex items-center justify-center">
+                <AiOutlineArrowRight className="text-2xl" />
+              </div>
+            </div>
+            <div className="flex items-center justify-around mt-5">
+              <BsFileEarmarkBarGraph className="text-3xl" />
+              <div>
+                <h5 className="text-lg font-semibold">Company Details</h5>
+                <p className="text-[#A2A2A2]">Download</p>
+              </div>
+              <div className="w-14 h-14 bg-[#FF3811] rounded flex items-center justify-center">
+                <AiOutlineArrowRight className="text-2xl" />
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#151515] text-[#FFF] p-10 rounded-xl my-8 text-center">
+            <img className="mx-auto" src={logo} alt="" />
+            <h4 className="text-xl font-bold my-8 leading-9">
+              Need Help? We Are Here <br /> To Help You
+            </h4>
+            <div className=" bg-[#FFF] pt-5 pb-12 rounded-lg">
+              <h4 className="text-[#FF3811] text-xl font-bold">
+                Car Doctor <span className="text-[#151515]">Special</span>
+              </h4>
+              <p className="text-[#737373] font-bold mt-2">
+                Save up to <span className="text-[#FF3811]">60% off</span>
+              </p>
+            </div>
+            <h4 className="py-4 px-8 bg-[#FF3811] w-40 -mt-7 mx-auto rounded">Get A Quote</h4>
+          </div>
+          <h1 className="text-[#151515] text-4xl font-bold my-5">Price ${price}</h1>
+          <button className="btn btn-block bg-[#FF3811] border border-[#FF3811] text-white hover:border hover:border-[#FF3811] hover:text-[#FF3811] text-lg font-semibold duration-300">
+            Proceed Checkout
+          </button>
         </div>
       </div>
     </div>
